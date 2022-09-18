c020fb942d5b454df836d00790f55a44d9f59486
Сергей <sergemartin@yandex.ru>
2022-09-18 11:42:40 +0300

    добавил рандом

--- exsample003_sum/Program.cs
+++ exsample003_sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int x = 10;
-int y = 2;
+﻿int x = new Random().Next(1, 10);// числа от 1 до 9
+Console.WriteLine(x);// тут показываем x
+int y = new Random().Next(1, 10);// числа от 1 до 9
+Console.WriteLine(y);// тут показываем y 
 int a = x + y;
 Console.Write("сумма ");
 Console.Write(a);
