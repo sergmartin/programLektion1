3529025ecb56464011f033b061eaa7f168a0280a
Сергей <sergemartin@yandex.ru>
2022-09-18 16:12:08 +0300

    создал 005 еслиИначе

---
+++ examples005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите свое имя: ");
+string username = Console.ReadLine();
+
+if ( username.ToLower() == "сергей")
+{
+    Console.WriteLine("Это же Серёга!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username + "!");
+}
