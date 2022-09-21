70302979f044e7d7dcd66af62df63f773c553eb8
Сергей <sergemartin@yandex.ru>
2022-09-21 13:17:15 +0300

    добывил 0002, 005, 007 и удалил 002 с ошибкой

--- example002_HelloUser/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.Write("Введите свое имя ");
-string x = Console.ReadLine();
-Console.Write("Привет ");
-Console.Write(x);
---
+++ example007_magik/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Clear();// очистка экрана
+//Console.SetCursorPosition(10, 4);// позиция курсора
+//Console.WriteLine("+");
+
+
+int xa = 40;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 60;
+int yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+
+Console.SetCursorPosition(xb, yb);
+
+
+int x = xa, y = ya;
+
+int count = 0;
+
+while (count < 10000)
+{
+    int ccc = new Random().Next(0, 3); 
+    if (ccc == 0)
+    {
+        x = (x+xa)/2;
+        y = (y+ya)/2;
+    }
+    if (ccc == 1 )
+    {
+        x = (x + xb)/2;
+        y = (y+yb)/2;
+    }
+    if (ccc == 2 )
+    {
+         x = (x + xc)/2;
+        y = (y+yc)/2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count+=1;
+}
